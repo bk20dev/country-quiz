1e4bfcee5f393c1733deab954a1bfd465efbe97c
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-07-28 16:03:31 +0200

    Added Results component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Countries from '../data/countries.json';
 import useQuestion from '../hooks/useQuestion';
 import Question from './Question';
+import Results from './Results';
 
 const Quiz = () => {
   const [countries, setCountries] = useState(Countries);
   const [question, nextQuestion] = useQuestion(countries);
+  const [finished, setFinished] = useState(false);
+  const score = useRef(0);
 
   useEffect(() => {
     setCountries(Countries);
   }, []);
 
   if (!question) return <div>Loading...</div>;
+
+  if (finished)
+    return (
+      <Results
+        score={score.current}
+        onRetryClicked={() => {
+          setFinished(false);
+          nextQuestion();
+          score.current = 0;
+        }}
+      />
+    );
 
   const { id, title, flag, answers, correct } = question;
 
@@ -22,7 +37,14 @@ const Quiz = () => {
       flag={flag}
       answers={answers}
       correct={correct}
-      onNextClicked={nextQuestion}
+      onNextClicked={(correct) => {
+        if (correct) {
+          score.current++;
+          nextQuestion();
+        } else {
+          setFinished(true);
+        }
+      }}
     />
   );
 };
---
+++ src/components/Results.jsx
@@ -0,0 +1,22 @@
+import WinnersImage from '../assets/winners.svg';
+
+const Results = ({ score, onRetryClicked }) => (
+  <div className="flex flex-col items-center text-center gap-16 bg-white p-8 rounded-3xl relative">
+    <img src={WinnersImage} alt="" className="w-60" />
+    <div>
+      <p className="text-5xl font-bold text-navy mb-4">Results</p>
+      <p className="text-lg text-navy">
+        You got <span className="text-4xl font-bold text-green">{score}</span>{' '}
+        correct answers
+      </p>
+    </div>
+    <button
+      className="border-2 border-navy font-semibold text-navy py-4 px-16 rounded-xl hover:border-gold hover:bg-gold hover:text-white"
+      onClick={onRetryClicked}
+    >
+      Try again
+    </button>
+  </div>
+);
+
+export default Results;
